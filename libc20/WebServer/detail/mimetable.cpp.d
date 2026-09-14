571031f49a76fcb0f1464fcb8f2b7e7e877d092e
mmichaelantonyxaviereee-spec <m.michaelantonyxaviereee@gmail.com>
2026-09-14 21:08:23 +0530

    Add files via upload

---
+++ libc20/WebServer/detail/mimetable.cpp.d
@@ -0,0 +1,4 @@
+.pio/build/esp32dev/libc20/WebServer/detail/mimetable.cpp.o: \
+ C:/Users/rifka/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
+ C:/Users/rifka/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
+ C:/Users/rifka/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
